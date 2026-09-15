4bfe491031ec9c2de136e4e48312ed6953ff70e3
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-09-15 12:52:55 +0900

    update

--- src/blockdata/jsonHashBlockDataMap.ts
+++ src/blockdata/jsonHashBlockDataMap.ts
@@ -1,19 +1,18 @@
 import { PlainHashBlockDataMap } from "./plainHashBlockDataMap";
 import { HashBlockDataMap } from "./types";
 
-export class JSONHashBlockDataMap
-  extends PlainHashBlockDataMap
-  implements
-    HashBlockDataMap<string | number | boolean | object | null | undefined>
-{
-  override *read(
+export class JSONHashBlockDataMap implements HashBlockDataMap<
+  string | number | boolean | object | null | undefined
+> {
+  constructor(private readonly plainHashBlockDataMap: PlainHashBlockDataMap) {}
+  *read(
     key: string,
   ): Generator<
     undefined,
     string | number | boolean | object | null | undefined,
     unknown
   > {
-    const result = yield* super.read(key);
+    const result = yield* this.plainHashBlockDataMap.read(key);
     if (result === undefined) return undefined;
     const json = JSON.parse(result)[key];
     return json;
